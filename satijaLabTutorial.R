07c7decd6fce88492f1c4b6e32040a4c28a45477
devenshah2 <deven@devenshah.com>
2023-04-21 15:25:50 -0700

    Setup the Seurat Object

---
+++ satijaLabTutorial.R
@@ -0,0 +1,21 @@
+library(dplyr)
+library(Seurat)
+library(patchwork)
+
+# Load the PBMC dataset
+pbmc.data <- Read10X(data.dir = "../data/pbmc3k/filtered_gene_bc_matrices/hg19/")
+# Initialize the Seurat object with the raw (non-normalized data).
+pbmc <- CreateSeuratObject(counts = pbmc.data, project = "pbmc3k", min.cells = 3, min.features = 200)
+pbmc
+
+# Lets examine a few genes in the first thirty cells
+pbmc.data[c("CD3D", "TCL1A", "MS4A1"), 1:30]
+
+dense.size <- object.size(as.matrix(pbmc.data))
+dense.size
+
+sparse.size <- object.size(pbmc.data)
+sparse.size
+
+dense.size/sparse.size
+
